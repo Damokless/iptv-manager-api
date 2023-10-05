9a55b949d569d1f8fc086d224465e25562e3fd4e
damokless <damokless@protonmail.com>
2023-10-05 18:36:46 +0200

    feat : adding comments

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-
+// modules imports
 import Fastify from 'fastify'
-import playlistToJson from './services/playlistToJson'
-import proxy from './services/playlistCheker'
 import fastifyCors from '@fastify/cors'
 
+// services
+import playlistToJson from './services/playlistToJson'
+import proxy from './services/playlistCheker'
+
+// fastify config
 const fastify = Fastify({logger: true})
 fastify.register(fastifyCors, {origin: '*'})
 
+// routes
 fastify.post('/playlistToJson', async (req) => {return playlistToJson(req.body)});
 fastify.post('/playlistChecker', async (req) => {return proxy(req.body)});
 
+// start server and listen port 4000
 const start = async () => {
   try {
     await fastify.listen({ port: 4000 })
